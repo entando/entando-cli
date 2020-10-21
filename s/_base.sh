1c7b6f7a670e7328f190e0f23a945884ebedf8ab
Walter Caffiero <w.caffiero@entando.com>
2020-10-21 22:45:13 +0200

    Improved the error management

--- s/_base.sh
+++ s/_base.sh
@@ -5,57 +5,76 @@
 ENT_HOME="$PWD"
 mkdir -p "$ENTANDO_ENT_ACTIVE/w"
 
-nvm_activate() {
-  NVM_DIR="$HOME/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" || return
-  export NVM_DIR
+# ----------------------------------------------------------------------------------------------------------------------
+# ERROR AND EXIT MANAGEMENT
+
+XU_STATUS_FILE="$ENTANDO_ENT_ACTIVE/w/.status"
+XU_BACKTRACE=""
+
+# PROGRAM STATUS
+xu_clear_status() {
+  [ "$XU_STATUS_FILE" != "" ] && [ -f "$XU_STATUS_FILE" ] && rm -- "$XU_STATUS_FILE"
 }
 
-# TRAPS
-function backtrace() {
-  xu_get_status
-  if [ "$XU_RES" != "FATAL" ] && [ "$XU_RES" != "USER-ERROR" ] && [ "$XU_RES" != "EXIT" ]; then
-    n=${#FUNCNAME[@]}
+xu_set_status() {
+  [ "$XU_STATUS_FILE" != "" ] && echo "$@" > "$XU_STATUS_FILE"
+}
 
-    echo "" 2>&1
-    echo "~~~" 2>&1
-    echo -e "> Error detected in: function \"${FUNCNAME[1]}\" and position $(caller), backtrace:" 2>&1
-
-    for ((i = 0; i < $((n - 1)); i++)); do
-      printf '%*s' "$((i * 2 + 2))" ' ' 2>&1
-      echo "${BASH_SOURCE[$((i))]}:${BASH_LINENO[$((i))]}" 2>&1
-    done
-
-    xu_set_status "EXIT"
+xu_get_status() {
+  XU_RES=""
+  if [ "$XU_STATUS_FILE" != "" ] && [ -f "$XU_STATUS_FILE" ]; then
+    XU_RES="$(cut "$XU_STATUS_FILE" -d':' -f1)"
   fi
+  return 0
 }
 
+xu_set_status "-"
+
+function trace_position() {
+  local fn="${FUNCNAME[1]}"
+  local ln="${BASH_LINENO[0]}"
+  local fl="${BASH_SOURCE[0]}"
+  local pre=""
+  [ -n "$1" ] && pre=" -- "
+  echo "> CODE-POSITION: $fl, line $ln -- $fn()${pre}$*" 2>&1
+}
+
+function error-trap() {
+  trap - ERR
+  xu_get_status
+  exec >&2
+  XU_BACKTRACE=""
+  case "$XU_RES" in
+    "FATAL" | "USER-ERROR" | "EXIT")
+      ;;
+    *)
+      ind="  "
+      i=0
+      while true; do
+        c="$(caller $i)"
+        i="$((i + 1))"
+        [ -z "$c" ] && break
+        IFS=' ' read -r -a arr <<< "$c"
+        XU_BACKTRACE+="${ind}at: ${arr[2]}, line ${arr[0]} -- ${arr[1]}()\n"
+      done
+
+      xu_set_status "EXIT-ERR"
+      ;;
+  esac
+}
+trap error-trap ERR
 set -o errtrace
-trap backtrace ERR
-
-# UTILS
-. s/utils.sh
-. s/sys-utils.sh
-. s/var-utils.sh
-. s/logger.sh
-
-# ENVIRONMENT
-$SYS_OS_UNKNOWN && FATAL "Unsupported operating system"
-
-mkdir -p "$ENTANDO_ENT_ACTIVE/w"
-mkdir -p "$ENTANDO_ENT_ACTIVE/d"
-mkdir -p "$ENTANDO_ENT_ACTIVE/lib"
-
-. s/_conf.sh
-
-ENT_RUN_TMP_DIR=$(mktemp /tmp/ent.run.XXXXXXXXXXXX)
-[[ ! "$ENT_RUN_TMP_DIR" =~ /tmp/ ]] && {
-  # keep this as simple as possible, only native commands
-  echo "Internal Error: Unable to create the tmp dir" 2>&1
-  exit 99
-}
 
 exit-trap() {
+  [ "$?" == 0 ] && return $?
+  trap - ERR EXIT
+
+  [ -n "$XU_BACKTRACE" ] && {
+    echo ""
+    echo "> Callers:"
+    echo -e "$XU_BACKTRACE"
+  }
+
   xu_get_status
   sz=$(stat -c "%s" "$ENT_RUN_TMP_DIR")
 
@@ -74,3 +93,37 @@ exit-trap() {
   fi
 }
 trap exit-trap EXIT
+
+# ----------------------------------------------------------------------------------------------------------------------
+# UTILS
+
+. s/utils.sh
+. s/sys-utils.sh
+. s/var-utils.sh
+. s/logger.sh
+
+# ----------------------------------------------------------------------------------------------------------------------
+# ENVIRONMENT
+
+$SYS_OS_UNKNOWN && FATAL "Unsupported operating system"
+
+mkdir -p "$ENTANDO_ENT_ACTIVE/w"
+mkdir -p "$ENTANDO_ENT_ACTIVE/d"
+mkdir -p "$ENTANDO_ENT_ACTIVE/lib"
+
+. s/_conf.sh
+
+ENT_RUN_TMP_DIR=$(mktemp /tmp/ent.run.XXXXXXXXXXXX)
+[[ ! "$ENT_RUN_TMP_DIR" =~ /tmp/ ]] && {
+  # keep this as simple as possible, only native commands
+  echo "Internal Error: Unable to create the tmp dir" 2>&1
+  exit 99
+}
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+nvm_activate() {
+  NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" || return
+  export NVM_DIR
+}
