05fe390b0c42f014f843295288fc2d7640e2b584
Walter Caffiero <w.caffiero@entando.com>
2020-10-30 20:29:15 +0100

    Fixed mail assertor and adde tests

---
+++ s/tests/var-utils-tests.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 "$OPT" && pwd)"
+
+
+test_asserters() {
+  echo "> test_asserters.."
+
+  OPT="silent"
+
+  assert_strict_id "STRICT ID" "object" "$OPT" || FATAL "failed! $LINENO"
+  assert_strict_id "STRICT ID" "my_object" "$OPT" && FATAL "failed! $LINENO"
+  assert_strict_id "STRICT ID" "my-object" "$OPT" && FATAL "failed! $LINENO"
+  assert_strict_id "STRICT ID" "myObject" "$OPT" || FATAL "failed! $LINENO"
+  assert_strict_id "STRICT ID" "MyObject" "$OPT" && FATAL "failed! $LINENO"
+  assert_strict_id "STRICT ID" "my object" "$OPT" && FATAL "failed! $LINENO"
+  assert_strict_id "STRICT ID" "my.Object" "$OPT" && FATAL "failed! $LINENO"
+
+  assert_id "ID" "object" "$OPT" || FATAL "failed! $LINENO"
+  assert_id "ID" "my_object" "$OPT" || FATAL "failed! $LINENO"
+  assert_id "ID" "my-object" "$OPT" && FATAL "failed! $LINENO"
+  assert_id "ID" "myObject" "$OPT" || FATAL "failed! $LINENO"
+  assert_id "ID" "MyObject" "$OPT" && FATAL "failed! $LINENO"
+  assert_id "ID" "my object" "$OPT" && FATAL "failed! $LINENO"
+  assert_id "ID" "my.Object" "$OPT" && FATAL "failed! $LINENO"
+
+  assert_spc_id "SPC ID" "object" "$OPT" || FATAL "failed! $LINENO"
+  assert_spc_id "SPC ID" "my_object" "$OPT" || FATAL "failed! $LINENO"
+  assert_spc_id "SPC ID" "my-object" "$OPT" && FATAL "failed! $LINENO"
+  assert_spc_id "SPC ID" "myObject" "$OPT" || FATAL "failed! $LINENO"
+  assert_spc_id "SPC ID" "MyObject" "$OPT" || FATAL "failed! $LINENO"
+  assert_spc_id "SPC ID" "my object" "$OPT" || FATAL "failed! $LINENO"
+  assert_spc_id "SPC ID" "my.Object" "$OPT" && FATAL "failed! $LINENO"
+
+  assert_dn "DN" "domain" "$OPT" || FATAL "failed! $LINENO"
+  assert_dn "DN" "domai@n" "$OPT" && FATAL "failed! $LINENO"
+  assert_dn "DN" "domain?" "$OPT" && FATAL "failed! $LINENO"
+  assert_dn "DN" "domain99" "$OPT" || FATAL "failed! $LINENO"
+  assert_dn "DN" "my_domain" "$OPT" || FATAL "failed! $LINENO"
+  assert_dn "DN" "my-domain" "$OPT" || FATAL "failed! $LINENO"
+  assert_dn "DN" "mydomain.example.com" "$OPT" && FATAL "failed! $LINENO"
+  assert_dn "DN" "myDomain" "$OPT" && FATAL "failed! $LINENO"
+  assert_dn "DN" "MyDomain" "$OPT" && FATAL "failed! $LINENO"
+  assert_dn "DN" "my domain" "$OPT" && FATAL "failed! $LINENO"
+  assert_dn "DN" "my.Domain" "$OPT" && FATAL "failed! $LINENO"
+
+  assert_fdn "FDN" "domain" "$OPT" && FATAL "failed! $LINENO"
+  assert_fdn "FDN" "d@omain" "$OPT" && FATAL "failed! $LINENO"
+  assert_fdn "FDN" "domain?" "$OPT" && FATAL "failed! $LINENO"
+  assert_fdn "FDN" "domain99" "$OPT" && FATAL "failed! $LINENO"
+  assert_fdn "FDN" "my_domain" "$OPT" && FATAL "failed! $LINENO"
+  assert_fdn "FDN" "my-domain" "$OPT" && FATAL "failed! $LINENO"
+  assert_fdn "FDN" "mydomain.example.com" "$OPT" || FATAL "failed! $LINENO"
+  assert_fdn "FDN" "my-d_omain.example.com" "$OPT" || FATAL "failed! $LINENO"
+  assert_fdn "FDN" "myDomain.example.com" "$OPT" && FATAL "failed! $LINENO"
+  assert_fdn "FDN" "myDomain.example.com" "$OPT" && FATAL "failed! $LINENO"
+  assert_fdn "FDN" "MyDomain.example.com" "$OPT" && FATAL "failed! $LINENO"
+  assert_fdn "FDN" "my domain.example.com" "$OPT" && FATAL "failed! $LINENO"
+
+  assert_url "URL" "domain" "$OPT" && FATAL "failed! $LINENO"
+  assert_url "URL" "http://example.com" "$OPT" || FATAL "failed! $LINENO"
+  assert_url "URL" "http://example.com?q=1#f" "$OPT" || FATAL "failed! $LINENO"
+
+  assert_email "MAIL" "domain" "$OPT" && FATAL "failed! $LINENO"
+  assert_email "MAIL" "domain@example.com" "$OPT" || FATAL "failed! $LINENO"
+  assert_email "MAIL" "asb.s@example.com" "$OPT" || FATAL "failed! $LINENO"
+  assert_email "MAIL" "asb.s@an.example.com" "$OPT" || FATAL "failed! $LINENO"
+  assert_email "MAIL" ".s@example.com" "$OPT" && FATAL "failed! $LINENO"
+
+  assert_ip "IP" "192.168.0.1" "$OPT" || FATAL "failed! $LINENO"
+  assert_ip "IP" "" "$OPT" && FATAL "failed! $LINENO"
+  assert_ip "IP" "...." "$OPT" && FATAL "failed! $LINENO"
+  assert_ip "IP" ".168.1.0" "$OPT" && FATAL "failed! $LINENO"
+  assert_ip "IP" "192.168..0" "$OPT" && FATAL "failed! $LINENO"
+  assert_ip "IP" "192.168.0." "$OPT" && FATAL "failed! $LINENO"
+  assert_ip "IP" "192.168.0" "$OPT" && FATAL "failed! $LINENO"
+  assert_ip "IP" "192.168.0.a1" "$OPT" && FATAL "failed! $LINENO"
+  assert_ip "IP" "192.168a.0.1" "$OPT" && FATAL "failed! $LINENO"
+}
+
+true
--- s/var-utils.sh
+++ s/var-utils.sh
@@ -63,7 +63,7 @@ set_nn_ip() { assert_ip "$1" "$2" && _set_nn "$@"; }
 ## ASSERTIONS
 
 assert_nn() {
-  local pre && [ ! -z "$XCLP_RUN_CONTEXT" ] && pre="In context \"$XCLP_RUN_CONTEXT\""
+  local pre && [ -n "$XCLP_RUN_CONTEXT" ] && pre="In context \"$XCLP_RUN_CONTEXT\""
   [ -z "$2" ] && [ "$3" != "silent" ] && _log_e 0 "${pre}Value $1 cannot be null" && exit 3
   return 0
 }
@@ -76,56 +76,65 @@ assert_lit() {
 }
 
 assert_id() {
-  _assert_regex_nn "$1" "$2" "^[a-z][a-zA-Z0-9_]*$" "identifier" "$3"
+  _assert_regex_nn "$1" "$2" "^[a-z][a-zA-Z0-9_]*$" "" "identifier" "$3"
 }
 
 assert_spc_id() {
-  _assert_regex_nn "$1" "$2" "^[a-zA-Z0-9_ ]*$" "identifier with spaces" "$3"
+  _assert_regex_nn "$1" "$2" "^[a-zA-Z0-9_ ]*$" "" "identifier with spaces" "$3"
 }
 
 assert_num() {
-  _assert_regex_nn "$1" "$2" "^[0-9]*$" "number" "$3"
+  _assert_regex_nn "$1" "$2" "^[0-9]*$" "" "number" "$3"
 }
 
 assert_strict_id() {
-  _assert_regex_nn "$1" "$2" "^[a-z][a-zA-Z0-9]*$" "strict identifier" "$3"
+  _assert_regex_nn "$1" "$2" "^[a-z][a-zA-Z0-9]*$" "" "strict identifier" "$3"
 }
 
 assert_dn() {
-  _assert_regex_nn "$1" "$2" "^[a-z][a-z0-9_-]*$" "domain name" "$3"
+  _assert_regex_nn "$1" "$2" "^[a-z][a-z0-9_-]*$" "" "single domain name" "$3"
 }
 
 assert_url() {
-  _assert_regex_nn "$1" "$2" '^(https?|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]' "url" "$3"
+  _assert_regex_nn "$1" "$2" '^(https?|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]' "" "url" "$3"
 }
 
 assert_email() {
-  _assert_regex_nn "$1" "$2" '^[^@\s]+@[^@\s\.]+\.[^@\.\s]+$' "email" "$3"
+  _assert_regex_nn "$1" "$2" '^[^.]+.*$' "" "email" "$3" || return $?
+  _assert_regex_nn "$1" "$2" \
+    '^[a-z0-9._-]+@[a-z0-9._-]+\.[a-z0-9._-]+$' \
+    '^[a-z0-9._-]+@[a-z0-9._-]+\.[a-z0-9._-]+\.[a-z0-9._-]+$' \
+    "email" "$3"
+  _assert_regex_nn "$1" "$2" '^[a-z0-9._-]+@[a-z0-9._-]+\.[a-z0-9._-]+$' "" "email" "$3"
 }
 
 assert_fdn() {
-  _assert_regex_nn "$1" "$2" "^[.a-z0-9_-]*$" "full domain" "$3"
+  _assert_regex_nn "$1" "$2" "\\." "" "full domain" "$3" || return $?
+  _assert_regex_nn "$1" "$2" "^([a-z0-9._-])+$" "" "full domain" "$3"
 }
 
 assert_ip() {
-  _assert_regex_nn "$1" "$2" "^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$" "ip address" "$3"
+  _assert_regex_nn "$1" "$2" "^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$" "" "ip address" "$3"
 }
 
 # GENERIC REGEX ASSERTION
 # - $1  var name
 # - $2  value
 # - $3  regex
-# - $4  var type description
-# - $5  if "silent" prints no error
+# - $4  alternative regex
+# - $5  var type description
+# - $6  if "silent" prints no error
 _assert_regex_nn() {
-  [ "$6" != "" ] && FATAL "Internal Error: Invalid function call"
-  assert_nn "$1" "$2" "$5"
+  [ "$7" != "" ] && FATAL "[_assert_regex_nn] Internal Error: Invalid function call "
+  assert_nn "$1" "$2" "$6"
   if [[ "$2" =~ $3 ]]; then
-    return 0
+    if [[ -n "$4" && "$2" =~ $4 ]]; then
+      return 0
+    fi
   else
-    if [ "$5" != "silent" ]; then
-      local pre && [ ! -z "$XCLP_RUN_CONTEXT" ] && pre="In context \"$XCLP_RUN_CONTEXT\""
-      _log_e 0 "${pre}Value of $1 ($2) is not a valid $4"
+    if [ "$6" != "silent" ]; then
+      local pre && [ -n "$XCLP_RUN_CONTEXT" ] && pre="In context \"$XCLP_RUN_CONTEXT\""
+      _log_e 0 "${pre}Value of $1 ($2) is not a valid $5"
     fi
     return 1
   fi
