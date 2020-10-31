46c3f3f8a02d12c773905ff700b662c51ba36f75
Walter Caffiero <w.caffiero@entando.com>
2020-11-01 00:45:13 +0100

    Support for blueprint and bundle-tool repository configuration from "dist/"

--- s/_conf.sh
+++ s/_conf.sh
@@ -1,23 +1,17 @@
-# REPO_CUSTOM_MODEL
-REPO_CUSTOM_MODEL_ADDR="https://github.com/entando-k8s/entando-k8s-custom-model.git"
-REPO_CUSTOM_MODEL_DIR="entando-k8s-custom-model"
-
-# REPO_QUICKSTART
-REPO_QUICKSTART_ADDR="https://github.com/entando-k8s/entando-helm-quickstart.git"
-REPO_QUICKSTART_DIR="entando-helm-quickstart"
-
-# MISC
-DEPL_SPEC_YAML_FILE="entando-deployment-specs.yaml"
-REQUIRED_HELM_VERSION_REGEX="3.2.*"
+REPO_GENERATOR_JHIPSTER_ENTANDO_ADDR="https://github.com/entando/entando-blueprint/"
+REPO_BUNDLECLI_ADDR="https://github.com/entando-k8s/entando-bundle-cli/"
 
 # UTILITIES CONFIGURATION
 XU_LOG_LEVEL=9
 
 # CONSTS
-C_ENTANDO_BLUEPRINT_REPO="https://github.com/entando/entando-blueprint"
 CFG_FILE="$ENTANDO_ENT_ACTIVE/w/.cfg"
 C_HOSTS_FILE="/etc/hosts"
 C_BUNDLE_DESCRIPTOR_FILE_NAME="descriptor.yaml"
+C_ENT_PRJ_FILE=".ent-prj"
+
+C_GENERATOR_JHIPSTER_ENTANDO_NAME="generator-jhipster-entando"
+C_ENTANDO_BUNDLE_TOOL_NAME="entando-bundle-tool"
 
 
 # More dynamic configurations
