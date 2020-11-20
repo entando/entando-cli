b48980698f82ad66a47c3dd089661d09761e0206
Walter Caffiero <w.caffiero@entando.com>
2020-11-20 20:46:37 +0100

    Update the entando bundle tool name (entando-bundler)

--- s/_conf.sh
+++ s/_conf.sh
@@ -18,7 +18,7 @@ C_ENT_STATE_FILE=".ent-state"
 C_GENERATOR_JHIPSTER_ENTANDO_NAME="generator-jhipster-entando"
 C_ENTANDO_BUNDLER_DIR="entando-bundle-tool"
 C_ENTANDO_BUNDLER_NAME="entando-bundler"
-C_ENTANDO_BUNDLE_BIN_NAME="entando-bundle"
+C_ENTANDO_BUNDLE_BIN_NAME="entando-bundler"
 C_QUICKSTART_DEFAULT_RELEASE="quickstart"
 
 C_ENTANDO_LOGO_FILE="res/entando.png"
