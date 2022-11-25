4ee937d96f74434090facb9e49610362857d9560
Walter Caffiero <w.caffiero@entando.com>
2022-11-25 15:41:21 +0100

    ENG-4358 ent commands for ECR fails due to ingress detection issues

--- s/utils.sh
+++ s/utils.sh
@@ -965,7 +965,7 @@ app-get-main-ingresses() {
   local JSON="$(_kubectl get ingresses.v1.networking.k8s.io -o json)"
   
   #~~~
-  local JQ=".items[] | select(.metadata.name==$(_str_quote "$ENTANDO_APPNAME-ingress")).spec | .tls // \"-\", .rules[0].host"
+  local JQ=".items[0] | select(.metadata.name=$(_str_quote "$ENTANDO_APPNAME-ingress")).spec | .tls[0].hosts[0] // \"-\", .rules[0].host"
   stdin_to_arr $'\n\r' OUT < <(_jq "$JQ" -r <<< "$JSON")
   
   #~~~
