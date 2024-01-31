0a816a760645028a4aebc515574488301461b723
antromeo <45900043+antromeo@users.noreply.github.com>
2024-01-31 17:43:42 +0100

    ENG-5452: adds the optional bundle-cli-version variable

--- utils/activate-ent.sh
+++ utils/activate-ent.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 set -eux
-yes | ent check-env develop --lenient
+
+echo "BUNDLE_CLI_VERSION: $BUNDLE_CLI_VERSION"
+
+if [ -n "$BUNDLE_CLI_VERSION" ] ; then
+    ent check-env develop --yes --entando-bundle-cli-version="$BUNDLE_CLI_VERSION" --lenient
+else
+    ent check-env develop --yes  --lenient
+fi
