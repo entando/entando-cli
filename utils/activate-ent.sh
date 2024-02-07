e8fe47f27f52d9920d97f24635a971f40409f3bc
antromeo <45900043+antromeo@users.noreply.github.com>
2024-02-07 17:53:56 +0100

    ENG-TBD: adds node version as a var

--- utils/activate-ent.sh
+++ utils/activate-ent.sh
@@ -2,9 +2,15 @@
 set -eux
 
 echo "BUNDLE_CLI_VERSION: $BUNDLE_CLI_VERSION"
+echo "NODE_VERSION: $NODE_VERSION"
 
-if [ -n "$BUNDLE_CLI_VERSION" ] ; then
-    ent check-env develop --yes --entando-bundle-cli-version="$BUNDLE_CLI_VERSION" --lenient
-else
-    ent check-env develop --yes  --lenient
+
+if [ -n "$BUNDLE_CLI_VERSION" ]; then
+    BUNDLE_CLI_FLAG="--entando-bundle-cli-version=$BUNDLE_CLI_VERSION"
 fi
+
+if [ -n "$NODE_VERSION" ]; then
+    NODE_VERSION_FLAG="--node-version=$NODE_VERSION"
+fi
+
+ent check-env develop --yes ${BUNDLE_CLI_FLAG:=} ${NODE_VERSION_FLAG:=} --lenient
